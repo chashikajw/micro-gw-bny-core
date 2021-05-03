21c56d884ae008ad1a4a2e37006b6deb75762c0b
chashikajw <chashikajw007@gmail.com>
2021-05-03 15:26:09 +0530

    improve bash script

--- deployment/createfiles.sh
+++ deployment/createfiles.sh
@@ -1,30 +1,34 @@
 #!/bin/sh
 
-version=1.0.0
+coreVersion=1.1.7-SNAPSHOT
+coreGroupId=org.example
+coreArtifactId=micro-gw-bny-core
 
 #Generate dockerfile and assembly files from template
 generate_files () {
     mvn archetype:generate \
       -DarchetypeGroupId=org.example \
       -DarchetypeArtifactId=micro-gw-bny-core \
-      -DarchetypeVersion=1.1.5-SNAPSHOT \
-      -DgroupId=$version \
-      -DartifactId=$version \
-      -Dclustername=cjwcluster \
+      -DarchetypeVersion=$coreVersion \
+      -DgroupId=$coreVersion \
+      -Dversion=$coreVersion \
+      -DartifactId=$coreVersion \
+      -DcustomVariable=$coreVersion \
       -DinteractiveMode=false
 
     echo "File generation successfully"
 }
 
 ### Check if a version folder is exist ###
-if [ -d $version ]; then
+if [ -d $coreVersion ]; then
   echo "Directory  DOES exists."
-  rm -R $version
+  rm -R $coreVersion
   generate_files
 else
   echo "Directory  DOES NOT exists."
   generate_files
 fi
 
-#deplopy those in app engine
+
+#deplopy those in app engine. Include app engine commands
 
